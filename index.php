885bfe65f994edd34a1a6a6638bec3585f3f8c50
Miguel Branco <mbranco@epicvortex.com>
2015-01-22 01:00:27 +0000

    Add header and slider.

--- index.php
+++ index.php
@@ -5,12 +5,30 @@
 <head>
 	<meta charset="utf-8">
 	<title>Epic Vortex</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, min-scale=1, max-scale=1">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/slick.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
+	<header class="cf">
+		<img class="logo" src="img/logo.png">
+		<nav class="cf">
+			<a href="sueca.php">Products</a>
+			<a href="team.php">Team</a>
+		</nav>
+	</header>
+
+	<article>
+		<div class="slider">
+			<div><img src="img/featured/1-wide.jpg"></div>
+			<div><img src="img/featured/2-wide.jpg"></div>
+		</div>
+	</article>
+
+
 
 	<script src="js/jquery-1.11.2.min.js"></script>
 	<script src="js/slick.min.js"></script>
---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	console.log('Slider');
+	$('.slider').slick();
+});
