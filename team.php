9e8fb87dfa329071f30a43c169e04474fdfddec6
Gonçalo Gomes <ggomes814@gmail.com>
2015-01-26 00:18:26 +0000

    - Added team and apps page skel

---
+++ team.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Epic Vortex</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="css/slick.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+  <header class="cf">
+    <img class="logo" src="img/logo.png">
+    <nav class="cf">
+      <a href="sueca.php">Products</a>
+      <a href="team.php">Team</a>
+    </nav>
+  </header>
+
+  <table style="width:100%">
+    <tr>
+      <td class="desc">Miguel Branco</td>
+      <td class="desc">Software Developer</td>
+      <td class="desc">Portugal</td>
+    </tr>
+    <tr>
+      <td class="desc">Gustaf Linnerborg</td>
+      <td class="desc">Designer</td>
+      <td class="desc">Sweden</td>
+    <tr>
+    </tr>
+      <td class="desc">Gonçalo Graças</td>
+      <td class="desc">Software Developer</td>
+      <td class="desc">Portugal</td>
+    </tr>
+  </table>
+
+  <script src="js/jquery-1.11.2.min.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+
+</html>
