b6851925669d081dbbd765599aa871b7da5a3b67
GGomes11 <ggomes814@gmail.com>
2015-02-15 22:44:34 +0000

    Add Linkedin profile(s) to 'Team' page

--- team.php
+++ team.php
@@ -5,7 +5,7 @@
 
 	<div class="cf">
 		<div class="left-div">
-			<h2>Miguel Branco</h2>
+			<h2>Miguel Branco	>> <a class="a-linkedin" href="https://www.linkedin.com/in/migueljcb"><i class="fa fa-linkedin-square"></i></a></h2>
 			<p>
 				Software developer with a passion for all things digital. Currently interested in Android, Web Development and tinkering with Pebble watch, Raspberry Pi, and Arduino. Also pretends to have time to plan and execute games.
 			</p>
@@ -16,7 +16,7 @@
 	</div>
 	<div class="cf">
 		<div class="right-div">
-			<h2>Gustaf Linnerborg</h2>
+			<h2>Gustaf Linnerborg	>> <a class="a-linkedin" href="#"><i class="fa fa-linkedin-square"></i></a></h2>
 			<p>
 				Designer Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor	incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 			</p>
@@ -27,7 +27,7 @@
 	</div>
 	<div class="cf">
 		<div class="left-div">
-			<h2>Gon&ccedil;alo Gra&ccedil;as</h2>
+			<h2>Gon&ccedil;alo Gra&ccedil;as >> <a class="a-linkedin" href="https://www.linkedin.com/profile/view?id=179389089"><i class="fa fa-linkedin-square"></i></a></h2>
 			<p>
 				Software engineer, whose main areas of expertise and interest are Software Engineering concepts, including Software design, development and testing. Additionally, has also great affinity and good knowledge about Ubiquitous and Mobile Computing as well as Distributed Systems.
 			</p>
