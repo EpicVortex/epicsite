774f217953de3e748b29947f393187487f264ad6
Miguel Branco <mbranco@epicvortex.com>
2015-04-04 21:43:40 +0100

    Save sueca logs when sent on body

--- sueca-log.php
+++ sueca-log.php
@@ -3,22 +3,33 @@
  * Accepts log files and stores them
  */
 
-if (isset($_FILES["file"])) {
-	$error = $_FILES["file"]["error"];
+file_put_contents('suecalogs.log', "Access at " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
+//file_put_contents('suecalogs.log', "SERVER: " . print_r($_SERVER, true) . "\n", FILE_APPEND);
+//file_put_contents('suecalogs.log', "GET: " . print_r($_GET, true) . "\n", FILE_APPEND);
+//file_put_contents('suecalogs.log', "POST: " . print_r($_POST, true) . "\n", FILE_APPEND);
+//file_put_contents('suecalogs.log', "FILES: " . print_r($_FILES, true) . "\n", FILE_APPEND);
+//file_put_contents('suecalogs.log', "BODY: " . @file_get_contents('php://input') . "\n", FILE_APPEND);
+//file_put_contents('suecalogs.log', "BODY2: " . $HTTP_RAW_POST_DATA . "\n", FILE_APPEND);
+
+if (!file_exists('sueca-logs')) {
+	mkdir('sueca-logs', 0777);
+}
+
+if (isset($_FILES['file'])) {
+	$error = $_FILES['file']['error'];
 	if ($error == 0) {
 		// File sent
-		$fileSize = $_FILES["file"]['size'];
-		$fileName = $_FILES["file"]['name'];
-		$tempFile = $_FILES["file"]['tmp_name'];
-
-		if (!file_exists('sueca-logs')) {
-			mkdir('sueca-logs', 0777);
-		}
-
-		$filename = 'sueca-logs/' . uniqid() . ".log";
+		$fileSize = $_FILES['file']['size'];
+		$fileName = $_FILES['file']['name'];
+		$tempFile = $_FILES['file']['tmp_name'];
+		$filename = 'sueca-logs/' . uniqid() . '.log';
 		$res = move_uploaded_file($tempFile, $filename);
-		echo "Moved file $filename: " . (int) $res . "<br>\n";
+		file_put_contents('suecalogs.log', "Moved file $filename: " . (int) $res . "<br>\n", FILE_APPEND);
 	}
+} else if ($HTTP_RAW_POST_DATA != '') {
+	$filename = 'sueca-logs/' . uniqid() . '.log';
+	file_put_contents($filename, $HTTP_RAW_POST_DATA . "\n", FILE_APPEND);
+	file_put_contents('suecalogs.log', "Saved file $filename\n", FILE_APPEND);
 }
 
 
