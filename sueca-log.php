1e53edaee12a60a1b09dc53bc4063bbd063e3b18
Miguel Branco <mbranco@epicvortex.com>
2017-11-26 14:38:40 +0000

    Comment out log code

--- sueca-log.php
+++ sueca-log.php
@@ -3,14 +3,18 @@
  * Accepts log files and stores them
  */
 
-file_put_contents('suecalogs.log', "Access at " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
+//file_put_contents('suecalogs.log', "Access at " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
+
+/*
 //file_put_contents('suecalogs.log', "SERVER: " . print_r($_SERVER, true) . "\n", FILE_APPEND);
 //file_put_contents('suecalogs.log', "GET: " . print_r($_GET, true) . "\n", FILE_APPEND);
 //file_put_contents('suecalogs.log', "POST: " . print_r($_POST, true) . "\n", FILE_APPEND);
 //file_put_contents('suecalogs.log', "FILES: " . print_r($_FILES, true) . "\n", FILE_APPEND);
 //file_put_contents('suecalogs.log', "BODY: " . @file_get_contents('php://input') . "\n", FILE_APPEND);
 //file_put_contents('suecalogs.log', "BODY2: " . $HTTP_RAW_POST_DATA . "\n", FILE_APPEND);
+*/
 
+/*
 if (!file_exists('sueca-logs')) {
 	mkdir('sueca-logs', 0777);
 }
@@ -31,7 +35,7 @@ if (isset($_FILES['file'])) {
 	file_put_contents($filename, $HTTP_RAW_POST_DATA . "\n", FILE_APPEND);
 	file_put_contents('suecalogs.log', "Saved file $filename\n", FILE_APPEND);
 }
-
+*/
 
 ?>
 
@@ -43,9 +47,9 @@ if (isset($_FILES['file'])) {
 		<title>Sueca log</title>
 	</head>
 	<body>
-		<form method="POST" action="sueca-log.php" enctype="multipart/form-data">
+		<!--<form method="POST" action="sueca-log.php" enctype="multipart/form-data">
 			<input type="file" name="file">
 			<input type="submit" value="Send log">
-		</form>
+		</form>-->
 	</body>
 </html>
